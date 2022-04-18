f91348ea59f1d6e8c63e05bb6d55425eeda7e520
daniel <da.rebolledo@duocuc.cl>
2022-04-17 23:25:14 -0400

    mejoras

---
+++ js/formRegistro.js
@@ -0,0 +1,89 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Debe ingresar un nombre de usuario');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Debe ingresar un Email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email no valido');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Debe ingresar una contraseña');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value === '') {
+        setErrorFor(password2, 'Debe ingresar contraseña');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Las contraseñas no coinciden');
+    } else {
+        setSuccessFor(password2);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
---
+++ js/formulario_inicio.js
@@ -0,0 +1,66 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Debe ingresar un nombre de usuario');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Debe ingresar una contraseña');
+    } else {
+        setSuccessFor(password);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
