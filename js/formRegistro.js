aa2fbe21035ebc6259292ef29cb8d39673165697
daniel <da.rebolledo@duocuc.cl>
2022-04-22 10:44:37 -0400

    act entrega 2

---
+++ js/formRegistro.js
@@ -0,0 +1,147 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+$('#form').on('submit', function(e) {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+username.addEventListener('input', () => {
+    checkName();
+});
+
+email.addEventListener('input', () => {
+    checkEmail();
+});
+
+password.addEventListener('input', () => {
+    checkPassword();
+});
+
+password2.addEventListener('input', () => {
+    checkPassword1();
+});
+
+function checkName() {
+    const usernameValue = username.value.trim();
+    if (usernameValue === '') {
+        setErrorFor(username, 'Debe ingresar un nombre de usuario');
+    } else {
+        setSuccessFor(username);
+    }
+}
+
+function checkEmail() {
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setErrorFor(email, 'Debe ingresar un Email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'No ingreso un email válido');
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+function checkPassword() {
+    const passwordValue = password.value.trim();
+    if (passwordValue === '') {
+        setErrorFor(password, 'Debe ingresar un comentario');
+    } else {
+        setSuccessFor(password);
+    }
+}
+
+function checkPassword1() {
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    if (password2Value === '') {
+        setErrorFor(password2, 'Debe ingresar contraseña');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Las contraseñas no coinciden');
+    } else {
+        setSuccessFor(password2);
+    }
+}
+
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Debe ingresar un nombre de usuario');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Debe ingresar un Email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email no valido');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Debe ingresar una contraseña');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value === '') {
+        setErrorFor(password2, 'Debe ingresar contraseña');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Las contraseñas no coinciden');
+    } else {
+        setSuccessFor(password2);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
--- js/main.js
+++ js/main.js
@@ -12,17 +12,60 @@ function toggleMenu() {
 }
 
 
-
+//Formulario de contacto
 const form = document.getElementById('form');
 const usuario = document.getElementById('username');
 const email = document.getElementById('email');
 const comentario = document.getElementById('comentario');
 
-form.addEventListener('submit', e => {
+
+$('#form').on('submit', function(e) {
     e.preventDefault();
 
     checkInputs();
+})
+
+usuario.addEventListener('input', () => {
+    checkName();
 });
+
+email.addEventListener('input', () => {
+    checkEmail();
+});
+
+comentario.addEventListener('input', () => {
+    checkComentario();
+});
+
+function checkName() {
+    const usuarioValue = usuario.value.trim();
+    if (usuarioValue === '') {
+        setErrorFor(usuario, 'Debe ingresar un nombre de usuario');
+    } else {
+        setSuccessFor(usuario);
+    }
+}
+
+function checkEmail() {
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setErrorFor(email, 'Debe ingresar un Email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'No ingreso un email válido');
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+function checkComentario() {
+    const comentarioValue = comentario.value.trim();
+    if (comentarioValue === '') {
+        setErrorFor(comentario, 'Debe ingresar un comentario');
+    } else {
+        setSuccessFor(comentario);
+    }
+}
+
 
 function checkInputs() {
     // trim to remove the whitespaces
@@ -66,3 +109,16 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
